9c925f6d893a502f27f6eb0875f1c7253f89931e
Sriram <sriramsriram123654@gmail.com>
2026-03-26 17:45:28 +0530

    soul

--- src/main/java/com/mainPack/controller/StudentController.java
+++ src/main/java/com/mainPack/controller/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import com.mainPack.entity.Student;	
 import com.mainPack.service.StudentService;
 import java.util.List;   
